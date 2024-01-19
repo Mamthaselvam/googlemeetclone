15061456c0c40b0a3644b73e03f363559d02b62f
Mamthaselvam <mamthapselvam@gmail.com>
2024-01-19 21:37:37 +0530

    added main left part

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,46 +1,54 @@
-import React from 'react'
+import React from "react";
 import { MdOutlineVideoCall } from "react-icons/md";
 import { LiaKeyboard } from "react-icons/lia";
 const Main = () => {
   return (
-    <div className='text-start font-sans '>
-        <div className='grid grid-cols-2 gap-4 '>
-           <div className='col-span-1 m-20 '>
-             <div >
-                <div className='mt-20 ml-10'>
-                   <h5 className='font-bold text-3xl'>Premium video meetings.</h5>
-                   <h5 className='font-bold text-3xl'>Now free for everyone.</h5>
-                </div>   
-                <div className='mt-10 ml-10' >
-                    <p className=''>We're-engineered the service we build for secure business </p>
-                    <p>meetings,Google Meet,to make it free and available for all. </p>
-                </div>
-                
-                <div className='flex  items-center justify-between'>
+    <div className="text-start font-sans ">
+      <div className="grid grid-cols-2 gap-4 ">
+        <div className="col-span-1 m-20 ">
+          <div>
+            <div className="mt-20 ml-10">
+              <h5 className="font-bold text-3xl">Premium video meetings.</h5>
+              <h5 className="font-bold text-3xl">Now free for everyone.</h5>
+            </div>
+            <div className="mt-10 ml-10">
+              <p className="">
+                We're-engineered the service we build for secure business{" "}
+              </p>
+              <p>
+                meetings,Google Meet,to make it free and available for all.{" "}
+              </p>
+            </div>
 
-                <button className=' flex items-center text-center cursor-pointer ml-10 mt-10 bg-blue-600 rounded text-white p-2' > 
-                   <span className='text-2xl'> < MdOutlineVideoCall/></span> 
-                    <span className='ml-2 text-sm'>New Meeting</span>
-                </button>
-                <button  >
-                <div className=' flex items-center text-center  mt-10  rounded border-dashed border-red-500'>
-                   <span> <LiaKeyboard/></span>
-                 <input type='text'  placeholder='Enter a code or link ' className='ml-3'/>
-                </div> 
-                </button>
-                <span className='mt-10 pr-5'>Join</span>
-                 
-               
-                </div>
-                
-             </div>
-           </div>
-           <div className='col-span-1'>
-                two
-           </div>
+            <div className="flex items-center ">
+              <button className=" flex items-center text-center cursor-pointer ml-10 mt-10 bg-blue-600 border text-white p-2">
+                <span className="text-2xl">
+                  {" "}
+                  <MdOutlineVideoCall />
+                </span>
+                <span className=" text-sm">New Meeting</span>
+              </button>
+              <button className="flex items-center">
+                <label className="flex items-center mt-10 ml-8 border border-slate-500 p-2 ">
+                  <span>
+                    <LiaKeyboard />
+                  </span>
+                  <input
+                    type="text"
+                    placeholder="Enter link or code "
+                    className="ml-2 outline-none"
+                  />
+                </label>
+                <span className="ml-3 mt-10 text-slate-300">Join</span>
+              </button>
+            </div>
+          </div>
+          <p className="ml-10 mt-10 cursor-pointer"><span className="text-blue-800 hover:underline">Learn more</span> about Google meet</p>
         </div>
+        <div className="col-span-1"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
